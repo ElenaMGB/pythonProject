9482c501f6913d1f5b0f0efac206210bfa70bcf3
ElenaMGB <161601927+ElenaMGB@users.noreply.github.com>
2024-09-07 16:51:46 +0500

    it works in russian

--- main.py
+++ main.py
@@ -9,15 +9,26 @@ import pprint
 from googletrans import Translator
 
 def get_russian_words():
-    url = "https://randomword.com/russian"
+    url = "https://randomword.com/"
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
-        russian_words = soup.find("div", id="random_word").text.strip()
+
+        english_words = soup.find("div", id="random_word").text.strip()
         word_definition = soup.find("div", id="random_word_definition").text.strip()
+
+        translator = Translator()
+        # russian_words = soup.find("div", id="random_word").text.strip()
+        # word_definition = soup.find("div", id="random_word_definition").text.strip()
+
+        russian_words = translator.translate(english_words, dest='ru').text
+        russian_definition = translator.translate(word_definition, dest='ru').text
         return {
+            "english_words": english_words,
+            "word_definition": word_definition,
+
             "russian_words": russian_words,
-            "word_definition": word_definition
+            "russian_definition": russian_definition
         }
     except:
         print("Произошла ошибка")
@@ -51,7 +62,7 @@ def word_game():
     while True:
         word_dict = get_russian_words()
         russian_words = word_dict.get("russian_words")
-        word_definition = word_dict.get("word_definition")
+        word_definition = word_dict.get("russian_definition")
 
         print(f"Значение слова - {word_definition}")
         user = input(f"Что это за слово? ")
